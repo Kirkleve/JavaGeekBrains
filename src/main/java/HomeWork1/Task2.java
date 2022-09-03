7eab4026bd9edbeb61eba49d11b8858f13c28bc4
Kirkleve <102994769+Kirkleve@users.noreply.github.com>
2022-09-03 16:10:11 +0200

    Comlite

--- src/main/java/HomeWork1/Task2.java
+++ src/main/java/HomeWork1/Task2.java
@@ -1,24 +1,26 @@
 package HomeWork1;
 
+import java.util.Arrays;
 import java.util.Objects;
 
 public class Task2 {
     public static void main(String[] args) {
         String[] array = {"aabb", "aabbb", "aaabb"};
-        String temp = " ";
-        int count = 0;
-
-        for (int i = 0; i < array.length; i++) {
-            for (int j = 0; j < array[i].length(); j++) {
-                if (temp.equals(array[j])){
-                    count++;
-                    System.out.println(count);
-                    System.out.println(array[j]);
-                }
-                temp = array[j];
-                System.out.println(temp);
-            }
-
+        System.out.print("In array " + Arrays.toString(array));
+        System.out.println(" longest prefix = " + longestPrefix(array));
+    }
+    private static String longestPrefix(String[] str) {
+        if (str.length == 0)
+            return "";
+        String prefix = str[0];
+        for (int i = 1; i < str.length; i++)
+            while (str[i].indexOf(prefix) != 0)
+        {
+            prefix = prefix.substring(0, prefix.length() - 1);
+            if (prefix.isEmpty())
+                return "";
         }
+        return prefix;
+
     }
 }
